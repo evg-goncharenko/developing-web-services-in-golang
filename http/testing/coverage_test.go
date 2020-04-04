24948be7b2114636ce49ad119d27d697b6059f12
Goncharenko4 <msu.goncharenko@yandex.ru>
2020-04-04 16:43:26 +0300

    Add testing

---
+++ http/testing/coverage_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+type TestCase struct {
+	Key     string
+	User    *User
+	IsError bool
+}
+
+func TestGetUser(t *testing.T) {
+	cases := []TestCase{
+		TestCase{"ok", &User{ID: 27}, false},
+		//TestCase{"fail", nil, true}, // для 100% покрытия
+		TestCase{"not_exist", nil, true},
+	}
+
+	for caseNum, item := range cases { // табличное тестирование
+		u, err := GetUser(item.Key)
+
+		if item.IsError && err == nil {
+			t.Errorf("[%d] expected error, got nil", caseNum)
+		}
+		if !item.IsError && err != nil {
+			t.Errorf("[%d] unexpected error: %v", caseNum, err)
+		}
+		if !reflect.DeepEqual(u, item.User) {
+			t.Errorf("[%d] wrong results: got %+v, expected %+v",
+				caseNum, u, item.User)
+		}
+	}
+
+}
+
+/*
+	go test -coverprofile=cover.out
+	go tool cover -html=cover.out -o cover.html
+*/
