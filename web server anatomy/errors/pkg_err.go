daa3b3b5d7d6a398ce23da3f725f07ed8dc3f3ba
Eugene Goncharenko <36198455+evg-goncharenko@users.noreply.github.com>
2020-04-23 01:44:10 +0300

    Add error processing

---
+++ web server anatomy/errors/pkg_err.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+var (
+	client = http.Client{Timeout: time.Duration(time.Millisecond)}
+)
+
+func getRemoteResource() error {
+	url := "http://127.0.0.1:9999/pages?id=123"
+	_, err := client.Get(url)
+	if err != nil {
+		return errors.Wrap(err, "resource error") // .Wrap - добавление к ошибки доп. информацию
+	}
+	return nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	err := getRemoteResource()
+	if err != nil {
+		fmt.Printf("full err: %+v\n", err)
+		// .Cause(err) - причина ошибки, .(type) - можем узнать тип ошибки (например, string)
+		switch err := errors.Cause(err).(type) {
+		case *url.Error: // Error - struct{Op, URL, Err}
+			fmt.Printf("resource %s err: %+v\n", err.URL, err.Err)
+			http.Error(w, "remote resource error", 500)
+		default:
+			fmt.Printf("%+v\n", err)
+			http.Error(w, "parsing error", 500)
+		}
+		return
+	}
+	w.Write([]byte("all is OK"))
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("starting server at :8080")
+	http.ListenAndServe(":8080", nil)
+}
